ca6eb1c512e129dd15e162f50488de090c467f20
DoyleKwak <wlwus123@naver.com>
2022-08-18 15:44:42 +0900

    commit 3

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.get('/',(req,res)=>{
+    res.send('Success!');
+})
 
 app.get('/kor', (req, res)=>{
     res.send('안녕하세요');
